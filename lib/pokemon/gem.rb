22eff915fccdc771008edca95d4dc2a8af539255
tmashuang <tmashuang@gmail.com>
2016-05-04 09:15:47 -0700

    Working on getting individual pokemon info

--- lib/pokemon/gem.rb
+++ lib/pokemon/gem.rb
@@ -2,7 +2,31 @@ require "pokemon/gem/version"
 require "unirest"
 
 module Pokemon
-  module Gem
-    # Your code goes here...
+  class Name
+    attr_reader :name
+    def initialize(options)
+      @name = options["name"]
+    end
+
+    def self.all
+      data = []
+      Unirest.get("http://pokeapi.co/api/v2/pokemon/?limit=151").body["results"].map {|pokemon| data << Name.new(pokemon)}
+      data.map {|pokemon| pokemon.name}
+    end
+
+    def self.name(name)
+      data = []
+      Unirest.get("http://pokeapi.co/api/v2/pokemon/?limit=151").body["results"].map {|pokemon| data << Name.new(pokemon)}
+      pokemon_names = data.map {|pokemon| pokemon.name}
+      pokemon_index = pokemon_names.index(name) + 1
+      self.class.get_pokemon(pokemon_index)
+    end
+
+private
+  def get_pokemon(pokemon_index)
+    Unirest.get("http://pokeapi.co/api/v2/pokemon/#{pokemon_index}").body
+  end
+
+
   end
 end
